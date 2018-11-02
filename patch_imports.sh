a697f412f2e942072cb491293d0ba46a4232764f
Alex F. Bokov <bokov@uthscsa.edu>
2018-11-02 09:21:30 -0500

    patch_imports.sh;maintainability;Script with hardcoded replacements to work around possible less bug #1931

---
+++ patch_imports.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# lessc is almost perfect, except... there is a wierd error message when I try to alter the 
+# path of a pre-existing external css import after replacing it with a variable. It sounds a
+# little like their issue here-- https://github.com/less/less.js/issues/1931
+# I will file a bug but in the meantime, leaving the pre-existing imports as-is in the less
+# files and then hardcoding the following transformations.
+
+# The leading parentheses are needed to avoid re-replacement
+sed -i "s|(vwQryTool|($remurl/vwQryTool|" webclient.test/js-i2b2/cells/CRC/assets/main_list.css;
+sed -i "s|(vwStatus|($remurl/vwStatus|" webclient.test/js-i2b2/cells/CRC/assets/main_list.css;
