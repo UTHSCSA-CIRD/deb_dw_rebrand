c5df44ce5a915169bf9cd0832fcd7387138eddab
Alex F. Bokov <bokov@uthscsa.edu>
2018-11-05 15:53:09 -0600

    mergewith.sh;bugfix;Now?

--- mergewith.sh
+++ mergewith.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # script to properly merge changes from other branches
 
-local mbranch=$1;
+mbranch=$1;
 
 source setup_for_editing.sh;
 
 # overwrite the auto-modified files
-for ii in ${ovrwrtfiles[*]}; do show "$mbranch:$ii" > "$ii"; done;
+for ii in ${ovrwrtfiles[*]}; do git show "$mbranch:$ii" > "$ii"; done;
 
 git merge --no-ff $mbranch;
 
