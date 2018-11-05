51f0630fc77a2c4042a6c37402cb701eef1ba477
Alex F. Bokov <bokov@uthscsa.edu>
2018-11-05 16:17:30 -0600

    mergewith.sh;propagation;Damn it's annoying when the script you're syncing is the one that handles syncing

--- mergewith.sh
+++ mergewith.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 # script to properly merge changes from other branches
 
-mbranch="$1";
+mbranch=${1:-'integration'};
+echo MERGING: $mbranch;
 
 source setup_for_editing.sh;
 
 # overwrite the auto-modified files
-for ii in ${ovrwrtfiles[*]}; do echo 'git show "$mbranch:$ii" > "$ii"'; git show "$mbranch:$ii" > "$ii"; done;
+for ii in ${ovrwrtfiles[*]}; do 
+  echo git show "$mbranch:$ii" \> "$ii"; 
+  git show "$mbranch:$ii" > "$ii"; 
+  git commit $ii -m "propagation from $mbranch";
+done;
+
 
 git merge --no-ff $mbranch -m "Propagating changes from $mbranch";
 
